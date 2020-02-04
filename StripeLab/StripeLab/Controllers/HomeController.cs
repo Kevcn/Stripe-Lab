a0d4d2bd5b5d09e0dfae15b4edeb8a3e645c94a4
ojassociates\Kevin.Li <kevin.li@ojassociates.com>
2020-02-04 11:02:53 +0000

    Test payment via Stripe gateway

---
+++ StripeLab/StripeLab/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Stripe;
+using StripeLab.Models;
+
+namespace StripeLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Charge(string stripeEmail, string stripeToken)
+        {
+            var customerService = new CustomerService();
+            var chargeService = new ChargeService();
+
+            var customer = customerService.Create(new CustomerCreateOptions { Email = stripeEmail, Source = stripeToken });
+            var charge = chargeService.Create(new ChargeCreateOptions { Amount = 500, Description = "Test payment", Currency = "GBP", Customer = customer.Id});
+
+            if (charge.Status == "succeeded")
+            {
+                var balanceTransactionId = charge.BalanceTransactionId;
+            }
+
+            return View();
+        }
+
+    }
+}
